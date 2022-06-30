d6be3f4d3a6f8a3e9fe928c619bf1f3790748488
Serhii <15111992ser@gmail.com>
2022-06-30 16:40:03 +0300

    add adaptive for cart

--- src/Components/CartItem/index.js
+++ src/Components/CartItem/index.js
@@ -1,8 +1,8 @@
 import styles from "./CartItem.module.css";
 import Counter from "../Counter";
-import {useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { removeItemIncart } from "../../redux/cartSlice";
-import {calcTotalPriceAndKgItems, removeFromTotalItem} from "../../redux/calcTotalSlice";
+import { calcTotalPriceAndKgItems, removeFromTotalItem } from "../../redux/calcTotalSlice";
 import { useCalcItem } from "../../hooks/useCalcItem";
 
 function CartItem({ name, price, srcImg, minOrder, sale }) {
@@ -11,11 +11,11 @@ function CartItem({ name, price, srcImg, minOrder, sale }) {
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.item}>
-        <div className={styles.imgBlock}>
-          <img src={srcImg} />
-          <span>{name}</span>
-        </div>
+      <div className={styles.imgBlock}>
+        <img src={srcImg} />
+        <span>{name}</span>
+      </div>
+      <div className={styles.content}>
         <div className={styles.counter}>
           <Counter minOrder={minOrder} totalObj={totalObj} calcSum={calcSum} />
           <div className={styles.price}>x {totalObj?.price || "0"} $/kg</div>
